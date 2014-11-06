859d3d682ef016c03e614a93c95460b5a5bb23e5
Ulrich Dobramysl <ulrich.dobramysl@vt.edu>
2014-11-06 07:35:54 +0000

    Changed module structure and added setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyGCM',
+      version='0.1',
+      description='Very simple Python Google Cloud Messaging HTTPS server',
+      author='Ulrich Dobramysl',
+      author_email='ulrich.dobramysl@gmail.com',
+      url='https://github.com/ulido/pyGCM',
+      py_modules=['gcm'],
+      )
